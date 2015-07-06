KeYProject/keyclipse

commit 1e5c71559e5d9764a200585407d013509e53ec91

[Fix] Duplicate generation of sort depending functions
Broke e.g. represents fields with satisfiability check

key/key.core/src/de/uka/ilkd/key/proof/init/AbstractOperationPO.java
@@ -46,6 +46,7 @@ import de.uka.ilkd.key.logic.Name;
 import de.uka.ilkd.key.logic.ProgramElementName;
 import de.uka.ilkd.key.logic.Sequent;
 import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.TermBuilder;
 import de.uka.ilkd.key.logic.label.SymbolicExecutionTermLabel;
 import de.uka.ilkd.key.logic.op.Equality;
 import de.uka.ilkd.key.logic.op.Function;
@@ -116,6 +117,7 @@ public abstract class AbstractOperationPO extends AbstractPO {
    private Term uninterpretedPredicate;
 
    protected InitConfig proofConfig;
+   protected TermBuilder tb;
 
    /**
     * Constructor.
@@ -153,8 +155,7 @@ public abstract class AbstractOperationPO extends AbstractPO {
    @Override
    public void readProblem() throws ProofInputException {
       assert proofConfig == null;
-      proofConfig = environmentConfig.deepCopy();
-      final Services proofServices = proofConfig.getServices();
+      final Services proofServices = postInit();
       final IProgramMethod pm = getProgramMethod();
       final boolean[] transactionFlags;
       final List<Term> termPOs = new ArrayList<Term>();
@@ -396,6 +397,13 @@ public abstract class AbstractOperationPO extends AbstractPO {
       // for JML annotation statements
       generateWdTaclets(proofConfig);
    }
+
+private Services postInit() {
+    proofConfig = environmentConfig.deepCopy();
+      final Services proofServices = proofConfig.getServices();
+      tb = proofServices.getTermBuilder();
+    return proofServices;
+}
 
    /**
     * Modifies the post condition with help of {@link POExtension#modifyPostTerm(InitConfig, Services, Term)}.

key/key.core/src/de/uka/ilkd/key/proof/init/AbstractPO.java
@@ -27,7 +27,6 @@ import de.uka.ilkd.key.ldt.HeapLDT;
 import de.uka.ilkd.key.logic.Choice;
 import de.uka.ilkd.key.logic.Namespace;
 import de.uka.ilkd.key.logic.Term;
-import de.uka.ilkd.key.logic.TermBuilder;
 import de.uka.ilkd.key.logic.op.Function;
 import de.uka.ilkd.key.logic.op.IObserverFunction;
 import de.uka.ilkd.key.logic.op.ProgramVariable;
@@ -54,7 +53,6 @@ import de.uka.ilkd.key.util.Pair;
  */
 public abstract class AbstractPO implements IPersistablePO {
 
-    protected final TermBuilder tb; 
     protected final InitConfig environmentConfig;
     protected Services environmentServices;
     protected final JavaInfo javaInfo;
@@ -75,7 +73,6 @@ public abstract class AbstractPO implements IPersistablePO {
                       String name) {
         this.environmentConfig = initConfig;
         this.environmentServices = initConfig.getServices();
-        this.tb = environmentServices.getTermBuilder();
         this.javaInfo = initConfig.getServices().getJavaInfo();
         this.heapLDT = initConfig.getServices().getTypeConverter().getHeapLDT();
         this.specRepos = initConfig.getServices().getSpecificationRepository();

key/key.core/src/de/uka/ilkd/key/proof/init/DependencyContractPO.java
@@ -26,6 +26,7 @@ import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.java.abstraction.KeYJavaType;
 import de.uka.ilkd.key.logic.Name;
 import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.TermBuilder;
 import de.uka.ilkd.key.logic.label.ParameterlessTermLabel;
 import de.uka.ilkd.key.logic.op.Function;
 import de.uka.ilkd.key.logic.op.IObserverFunction;
@@ -48,7 +49,8 @@ public final class DependencyContractPO extends AbstractPO
     
     private DependencyContract contract;
 
-   private InitConfig proofConfig;
+    private InitConfig proofConfig;
+    private TermBuilder tb;
            
     
     //-------------------------------------------------------------------------
@@ -153,9 +155,9 @@ public final class DependencyContractPO extends AbstractPO
           // FIXME: for some reason the above method call returns null now and then, the following line (hopefully) is a work-around
           if (target == null) target = contract.getTarget();
        }
-       proofConfig = environmentConfig.deepCopy();
-       final Services proofServices = proofConfig.getServices();
-
+       
+       final Services proofServices = postInit();
+       
        //prepare variables
        final ProgramVariable selfVar
        = !contract.getTarget().isStatic() 
@@ -271,6 +273,15 @@ public final class DependencyContractPO extends AbstractPO
 
        //add axioms
        collectClassAxioms(contract.getKJT(), proofConfig);
+    }
+
+
+
+    private Services postInit() {
+        proofConfig = environmentConfig.deepCopy();
+        final Services proofServices = proofConfig.getServices();
+        tb = proofServices.getTermBuilder();
+        return proofServices;
     }
 
 

key/key.core/src/de/uka/ilkd/key/proof/init/WellDefinednessPO.java
@@ -29,6 +29,7 @@ import de.uka.ilkd.key.ldt.HeapLDT;
 import de.uka.ilkd.key.logic.Name;
 import de.uka.ilkd.key.logic.Sequent;
 import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.TermBuilder;
 import de.uka.ilkd.key.logic.TermServices;
 import de.uka.ilkd.key.logic.label.ParameterlessTermLabel;
 import de.uka.ilkd.key.logic.op.Function;
@@ -68,6 +69,7 @@ public class WellDefinednessPO extends AbstractPO implements ContractPO {
     private final WellDefinednessCheck check;
     private Term mbyAtPre;
     private InitConfig proofConfig;
+    private TermBuilder tb;
 
     /**
      * Constructor
@@ -249,9 +251,8 @@ public class WellDefinednessPO extends AbstractPO implements ContractPO {
     public void readProblem() throws ProofInputException {
         assert proofConfig == null;
         
-        proofConfig = environmentConfig.deepCopy();
-        final Services proofServices = proofConfig.getServices();
-       
+        final Services proofServices = postInit();
+        
         final Variables vars = buildVariables(check, proofServices);
 
         register(vars, proofServices);
@@ -278,6 +279,13 @@ public class WellDefinednessPO extends AbstractPO implements ContractPO {
         collectClassAxioms(getKJT(), proofConfig);
 
         generateWdTaclets(proofConfig);
+    }
+
+    private Services postInit() {
+        proofConfig = environmentConfig.deepCopy();
+        final Services proofServices = proofConfig.getServices();
+        tb = proofServices.getTermBuilder();
+        return proofServices;
     }
 
     @Override

key/key.core/src/de/uka/ilkd/key/speclang/RepresentsAxiom.java
@@ -163,7 +163,7 @@ public final class RepresentsAxiom extends ClassAxiom {
 
     public ImmutableSet<Taclet> getTaclets(
           ImmutableSet<Pair<Sort, IObserverFunction>> toLimit,
-          Services services) {
+          final Services services) {
        List<LocationVariable> heaps = new ArrayList<LocationVariable>();
        int hc = 0;
        for(LocationVariable h : HeapContext.getModHeaps(services, false)) {

commit 6cea5d346dff49f6f548b65c825df13675182d8c

[Fix] InformationFlow implementation was broken after commit 1e5c71559e5d9764a200585407d013509e53ec91

key/key.core/src/de/uka/ilkd/key/informationflow/po/AbstractInfFlowPO.java
@@ -39,12 +39,11 @@ public abstract class AbstractInfFlowPO extends AbstractOperationPO implements I
 
         return proof;
     }
-    
+
     public InfFlowProof createProofObject(String proofName,
                                    String proofHeader,
                                    Term poTerm,
                                    InitConfig proofConfig) {
-        
         final InfFlowProof proof = new InfFlowProof(proofName,
                 poTerm,
                 proofHeader,

key/key.core/src/de/uka/ilkd/key/informationflow/po/BlockExecutionPO.java
@@ -84,6 +84,8 @@ public class BlockExecutionPO extends AbstractInfFlowPO
 
     @Override
     public void readProblem() throws ProofInputException {
+        final Services proofServices = postInit();
+
         // generate snippet factory for symbolic execution
         BasicPOSnippetFactory symbExecFactory =
                 POSnippetFactory.getBasicFactory(contract, symbExecVars,

key/key.core/src/de/uka/ilkd/key/informationflow/po/InfFlowContractPO.java
@@ -81,8 +81,7 @@ public class InfFlowContractPO extends AbstractInfFlowPO
     public void readProblem() throws ProofInputException {
         assert proofConfig == null;
 
-        proofConfig = environmentConfig.deepCopy();
-        Services proofServices = proofConfig.getServices();
+        final Services proofServices = postInit();
 
         // create proof obligation
         InfFlowPOSnippetFactory f =

key/key.core/src/de/uka/ilkd/key/informationflow/po/LoopInvExecutionPO.java
@@ -103,6 +103,8 @@ public class LoopInvExecutionPO extends AbstractInfFlowPO
 
     @Override
     public void readProblem() throws ProofInputException {
+        final Services proofServices = postInit();
+
         // generate snippet factory for symbolic execution
         BasicPOSnippetFactory symbExecFactory =
                 POSnippetFactory.getBasicFactory(loopInvariant, symbExecVars,
@@ -137,8 +139,8 @@ public class LoopInvExecutionPO extends AbstractInfFlowPO
         LoopInvExecutionPO lPO = (LoopInvExecutionPO) po;
         return loopInvariant.equals(lPO.loopInvariant);
     }
-    
-    public LoopInvariant getLoopInvariant() {        
+
+    public LoopInvariant getLoopInvariant() {
         return loopInvariant;
     }
 

key/key.core/src/de/uka/ilkd/key/informationflow/po/SymbolicExecutionPO.java
@@ -83,6 +83,8 @@ public class SymbolicExecutionPO extends AbstractInfFlowPO
 
     @Override
     public void readProblem() throws ProofInputException {
+        final Services proofServices = postInit();
+
         // generate snippet factory for symbolic execution
         BasicPOSnippetFactory symbExecFactory =
                 POSnippetFactory.getBasicFactory(contract, symbExecVars,

key/key.core/src/de/uka/ilkd/key/proof/init/AbstractOperationPO.java
@@ -398,12 +398,12 @@ public abstract class AbstractOperationPO extends AbstractPO {
       generateWdTaclets(proofConfig);
    }
 
-private Services postInit() {
-    proofConfig = environmentConfig.deepCopy();
+   protected Services postInit() {
+      proofConfig = environmentConfig.deepCopy();
       final Services proofServices = proofConfig.getServices();
       tb = proofServices.getTermBuilder();
-    return proofServices;
-}
+      return proofServices;
+   }
 
    /**
     * Modifies the post condition with help of {@link POExtension#modifyPostTerm(InitConfig, Services, Term)}.