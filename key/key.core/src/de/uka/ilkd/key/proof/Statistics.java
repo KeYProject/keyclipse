KeYProject/keyclipse

commit c718313caa629d4dc278f6433df33ede05dd9139

Made some methods shorter

key/key.core/src/de/uka/ilkd/key/proof/Statistics.java
@@ -99,9 +99,7 @@ public class Statistics {
         int tmpBlock = 0; // block and loop contract apps
         int tmpInv = 0; // loop invariants
 
-        while (it.hasNext()) {
-            tmpNodes++;
-            final Node node = it.next();
+        for (final Node node = it.next(); it.hasNext(); tmpNodes++) {
             final int c = node.childrenCount();
             if (c > 1) {
                 tmpBranches += c - 1;
@@ -109,10 +107,8 @@ public class Statistics {
 
             if (node.getNodeInfo().getInteractiveRuleApplication()) {
                 tmpInteractive++;
-
                 final String ruleAppName =
                         node.getAppliedRuleApp().rule().name().toString();
-
                 if (!interactiveAppsDetails.containsKey(ruleAppName)) {
                     interactiveAppsDetails.put(ruleAppName, 1);
                 } else {
@@ -148,8 +144,8 @@ public class Statistics {
                 } else if (ruleApp instanceof MergeRuleBuiltInRuleApp) {
                     tmpMergeApps++;
                 } else if (ruleApp instanceof TacletApp) {
-                    final de.uka.ilkd.key.rule.Taclet t = ((TacletApp)ruleApp).taclet();
-                    final String tName = t.name().toString();
+                    final String tName =
+                            ((TacletApp)ruleApp).taclet().name().toString();
                     if (tName.startsWith("allLeft")
                             || tName.startsWith("exRight")
                             || tName.startsWith("inst")) {

key/key.core/src/de/uka/ilkd/key/proof/init/FunctionalBlockContractPO.java
@@ -41,7 +41,7 @@ import de.uka.ilkd.key.util.MiscTools;
  */
 public class FunctionalBlockContractPO extends AbstractPO implements ContractPO {
 
-    public static Map<Boolean, String> TRANSACTION_TAGS =
+    private static final Map<Boolean, String> TRANSACTION_TAGS =
             new LinkedHashMap<Boolean, String>();
 
     private FunctionalBlockContract contract;
@@ -112,14 +112,44 @@ public class FunctionalBlockContractPO extends AbstractPO implements ContractPO 
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((contract == null) ? 0 : contract.hashCode());
+        result = prime * result
+                + ((environmentConfig == null) ?
+                        0 : environmentConfig.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
         if (!(obj instanceof FunctionalBlockContractPO)) {
             return false;
         }
-
         FunctionalBlockContractPO other = (FunctionalBlockContractPO) obj;
-        return contract.equals(other.contract)
-                && environmentConfig.equals(other.environmentConfig);
+        if (contract == null) {
+            if (other.contract != null) {
+                return false;
+            }
+        } else if (!contract.equals(other.contract)) {
+            return false;
+        }
+        if (environmentConfig == null) {
+            if (other.environmentConfig != null) {
+                return false;
+            }
+        } else if (!environmentConfig.equals(other.environmentConfig)) {
+            return false;
+        }
+        return true;
     }
 
     @Override

key/key.core/src/de/uka/ilkd/key/rule/BlockContractBuilders.java
@@ -76,7 +76,7 @@ import de.uka.ilkd.key.util.MiscTools;
 /**
  * This contains various builders used in building formulae and terms for block and loop contracts.
  */
-public class BlockContractBuilders {
+public final class BlockContractBuilders {
 
     public static final String ANON_IN_PREFIX = "anonIn_";
     public static final String ANON_OUT_PREFIX = "anonOut_";

key/key.core/src/de/uka/ilkd/key/rule/BlockContractExternalRule.java
@@ -46,7 +46,7 @@ import de.uka.ilkd.key.util.MiscTools;
  *
  * @see BlockContractExternalBuiltInRuleApp
  */
-public class BlockContractExternalRule extends AbstractBlockContractRule {
+public final class BlockContractExternalRule extends AbstractBlockContractRule {
 
     public static final BlockContractExternalRule INSTANCE = new BlockContractExternalRule();
 

key/key.core/src/de/uka/ilkd/key/rule/BlockContractInternalRule.java
@@ -54,7 +54,7 @@ import de.uka.ilkd.key.util.MiscTools;
  *
  * @see BlockContractInternalBuiltInRuleApp
  */
-public class BlockContractInternalRule extends AbstractBlockContractRule {
+public final class BlockContractInternalRule extends AbstractBlockContractRule {
 
     public static final BlockContractInternalRule INSTANCE = new BlockContractInternalRule();
 

key/key.core/src/de/uka/ilkd/key/rule/LoopContractExternalRule.java
@@ -44,7 +44,7 @@ import de.uka.ilkd.key.util.MiscTools;
  *
  * @see LoopContractExternalBuiltInRuleApp
  */
-public class LoopContractExternalRule extends AbstractLoopContractRule {
+public final class LoopContractExternalRule extends AbstractLoopContractRule {
 
     public static final LoopContractExternalRule INSTANCE = new LoopContractExternalRule();
 

key/key.core/src/de/uka/ilkd/key/rule/LoopContractInternalRule.java
@@ -38,7 +38,7 @@ import de.uka.ilkd.key.util.MiscTools;
  *
  * @see LoopContractInternalBuiltInRuleApp
  */
-public class LoopContractInternalRule extends AbstractLoopContractRule {
+public final class LoopContractInternalRule extends AbstractLoopContractRule {
 
     public static final LoopContractInternalRule INSTANCE = new LoopContractInternalRule();
 

commit 58afdc8bcb38141f0ba4054f85fe336781188332

Fixed an infinite loop in Statistics.java
(introduced in c718313)

key/key.core/src/de/uka/ilkd/key/proof/Statistics.java
@@ -11,10 +11,10 @@ import de.uka.ilkd.key.rule.AbstractBlockSpecificationElementBuiltInRuleApp;
 import de.uka.ilkd.key.rule.ContractRuleApp;
 import de.uka.ilkd.key.rule.LoopInvariantBuiltInRuleApp;
 import de.uka.ilkd.key.rule.OneStepSimplifier.Protocol;
-import de.uka.ilkd.key.rule.merge.MergeRuleBuiltInRuleApp;
 import de.uka.ilkd.key.rule.RuleApp;
 import de.uka.ilkd.key.rule.TacletApp;
 import de.uka.ilkd.key.rule.UseDependencyContractApp;
+import de.uka.ilkd.key.rule.merge.MergeRuleBuiltInRuleApp;
 import de.uka.ilkd.key.util.EnhancedStringBuffer;
 import de.uka.ilkd.key.util.Pair;
 
@@ -99,7 +99,11 @@ public class Statistics {
         int tmpBlock = 0; // block and loop contract apps
         int tmpInv = 0; // loop invariants
 
-        for (final Node node = it.next(); it.hasNext(); tmpNodes++) {
+        Node node;
+        while (it.hasNext()) {
+            node = it.next();
+            tmpNodes++;
+
             tmpBranches = addTmpBranches(node, tmpBranches);
             tmpInteractive =
                     addInteractiveRuleApps(node, interactiveAppsDetails, tmpInteractive);